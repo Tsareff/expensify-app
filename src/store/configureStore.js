314fa5dc1d11c2219ef4145539c9c6424fc8c61c
Tsareff <tsareff.sergey@gmail.com>
2018-09-26 13:18:53 +0300

    Setup test database env

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,17 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses';
 import filterReducer from '../reducers/filters';
 
 //Store creation
+
+const composeEnchancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => {
   const store = createStore(
     combineReducers({ expenses: expensesReducer, filters: filterReducer }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeEnchancers(applyMiddleware(thunk)),
   );
   return store;
 };
